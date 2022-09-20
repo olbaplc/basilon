0d7295a734114f7f070d1a6d0847d416a5860c35
olbaplc <olbaplc@gmail.com>
2022-09-20 21:44:45 +0200

    he ponido el lessgo

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var audio = new Audio("letsgo.wav");
+var audio = new Audio("lessgo.mp3");
 
 document.onclick = function() {
   audio.play();
 }
 
 
-var sound = new Audio("letsgo.wav");
+var sound = new Audio("lessgo.mp3");
 
 button.addEventListener('click',()=>{
 	sound.currentTime = 4;
