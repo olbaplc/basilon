fa24726faa8c7b5558c5381b100fd023e49307a5
olbaplc <olbaplc@gmail.com>
2022-09-23 13:55:38 +0200

    he comentado el js, al final es molesto si no lo controlo

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var audio = new Audio("lessgo.mp3");
+//var audio = new Audio("lessgo.mp3");
 
-document.onclick = function() {
-  audio.play();
-}
+//document.onclick = function() {
+//  audio.play();
+//}
 
 
-var sound = new Audio("lessgo.mp3");
+//var sound = new Audio("lessgo.mp3");
 
-button.addEventListener('click',()=>{
-	sound.currentTime = 4;
-	sound.play();
-});
+//button.addEventListener('click',()=>{
+//	sound.currentTime = 4;
+//	sound.play();
+//});
